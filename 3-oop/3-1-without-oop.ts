b4bffb8338437591dee43fdc8365a247a36e6e16
yurim Kim <icho0405@gmail.com>
2021-09-10 08:56:43 +0900

    add: 절차지향적으로 커피기계 만들기 💡

--- 3-oop/3-1-without-oop.ts
+++ 3-oop/3-1-without-oop.ts
@@ -1,26 +1,28 @@
 // 절차지향적으로 커피기계 만들기 💡
-// {
-//   type CoffeeCup = {
-//     shots: number;
-//     hasMilk: boolean;
-//   };
 
-//   const BEANS_GRAMM_PER_SHOT: number = 7;
+{
+  type CoffeeCup = {
+    shots: number,
+    hasMilk: boolean
+  }
 
-//   let coffeeBeans: number = 0;
-//   function makeCoffee(shots: number): CoffeeCup {
-//     if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
-//       throw new Error('Not enough coffee beans!');
-//     }
-//     coffeeBeans -= shots * BEANS_GRAMM_PER_SHOT;
-//     return {
-//       shots,
-//       hasMilk: false,
-//     };
-//   }
+  const BEANS_GRAMM_PER_SHOT: number = 7;
 
-//   coffeeBeans += 3 * BEANS_GRAMM_PER_SHOT;
-//   const coffee = makeCoffee(2);
-//   console.log(coffee);
-// }
+  let coffeeBeans: number = 0;
+  function makeCoffee(shots: number): CoffeeCup {
+    if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
+      throw new Error('Not enough coffee beans!')
+    }
+    coffeeBeans -= shots * BEANS_GRAMM_PER_SHOT;
+    return {
+      shots,  // key와 vlaue가 동일하다면 생략가능 shots: shots ⥤ shots
+      hasMilk: false
+    }
+  }
+
+  coffeeBeans += 3 * BEANS_GRAMM_PER_SHOT;
+  const coffee = makeCoffee(2);
+  console.log(coffee);
+}
+
 
