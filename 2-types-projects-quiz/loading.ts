429107a0a0cb059d0a45aaa680048ae329fbb4a9
yurim Kim <icho0405@gmail.com>
2021-09-10 08:04:38 +0900

    add: 2-types-projects-quiz

---
+++ 2-types-projects-quiz/loading.ts
@@ -0,0 +1,40 @@
+{
+  /**
+   * Print Loading State
+   */
+  type LoadingState = {
+    state: 'loading';
+  };
+
+  type SuccessState = {
+    state: 'success';
+    response: {
+      body: string;
+    };
+  };
+
+  type FailState = {
+    state: 'fail';
+    reason: string;
+  };
+
+  type ResourceLoadState = LoadingState | SuccessState | FailState;
+  const printLoginState = (state: ResourceLoadState) => {
+    switch (state.state) {
+      case 'loading':
+        console.log('👀 loading...')
+        break;
+      case 'success':
+        console.log(`😃 ${state.response.body}`)
+        break
+      case 'fail':
+        console.log(`😱 ${state.reason}`)
+        break;
+      default: throw new Error(`unknown state ${state}`)
+    }
+  }
+
+  printLoginState({ state: 'loading' }); // 👀 loading...
+  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
+  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+}
---
+++ main.ts
@@ -0,0 +1,8 @@
+console.log('hello World!!!');
+
+class Car {
+  engine : number;
+  constructor() {
+    this.engine = 1;
+  }
+}
