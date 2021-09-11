e91bd69fc7fbf1a681e19394035496eb16a43423
yurim Kim <icho0405@gmail.com>
2021-09-11 11:30:19 +0900

    add: Encapsulation 캡슐화 시켜보기 💡

--- 3-oop/3-3-encapsulation.ts
+++ 3-oop/3-3-encapsulation.ts
@@ -1,62 +1,47 @@
 {
+  // Encapsulation 캡슐화 시켜보기 💡
+  // 외부에서 설정하면 안되는 것들을 안보이게 하기
+  // 세 개의 키워드로 각각의 레벨을 지정할 수 있는데
+  // public: 기본값. 기본값이므로 생략 가능
+  // private: 외부에서 접근 불가. class를 상속받은 자식에서도 접근 불가.
+  // protected: 외부에서 접근 불가. 상속받은 자식에서는 접근 가능
+
   type CoffeeCup = {
-    shots: number;
-    hasMilk: boolean;
-  };
-
-  // public
-  // private
-  // protected
+    shots: number,
+    hasMilk: boolean
+  }
   class CoffeeMaker {
-    private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
-    private coffeeBeans: number = 0; // instance (object) level
+    private static BEANS_GRAMM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
 
-    private constructor(coffeeBeans: number) {
+    private constructor(coffeeBeans: number) { // construnctor함수에 private 키워드를 사용할 경우, new 키워드로 instance 생성이 불가해진다.
       this.coffeeBeans = coffeeBeans;
     }
 
     static makeMachine(coffeeBeans: number): CoffeeMaker {
-      return new CoffeeMaker(coffeeBeans);
+      return new CoffeeMaker(coffeeBeans)
     }
 
-    fillCoffeeBeans(beans: number) {
-      if (beans < 0) {
-        throw new Error('value for beans should be greater than 0');
+    fillCoffeeBeans(beens: number) {
+      if (beens < 0) {
+        throw new Error('vlaue for beans should be greater than 0')
       }
-      this.coffeeBeans += beans;
+      this.coffeeBeans += beens;
     }
 
     makeCoffee(shots: number): CoffeeCup {
       if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
-        throw new Error('Not enough coffee beans!');
+        throw new Error('Not enough coffee beans!')
       }
       this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
       return {
         shots,
-        hasMilk: false,
-      };
-    }
-  }
-
-  const maker = CoffeeMaker.makeMachine(32);
-  maker.fillCoffeeBeans(32);
-
-  class User {
-    get fullName(): string {
-      return `${this.firstName} ${this.lastName}`;
-    }
-    private internalAge = 4;
-    get age(): number {
-      return this.internalAge;
-    }
-    set age(num: number) {
-      if (num < 0) {
+        hasMilk: false
       }
-      this.internalAge = num;
     }
-    constructor(private firstName: string, public lastName: string) {}
   }
-  const user = new User('Steve', 'Jobs');
-  user.age = 6;
-  console.log(user.fullName);
+
+  // const maker = new CoffeeMaker(32); //constructor가 private level로 되었기 때문에 new 키워드를 사용하면 에러 발생
+  const maker = CoffeeMaker.makeMachine(32);
+  console.log(maker);
 }
