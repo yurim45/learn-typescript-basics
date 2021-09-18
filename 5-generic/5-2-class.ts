86243db3435874a786da79a566462332131af588
yurim Kim <icho0405@gmail.com>
2021-09-19 08:33:24 +0900

    add: 클래스를 제네릭 하게

--- 5-generic/5-2-class.ts
+++ 5-generic/5-2-class.ts
@@ -1,11 +1,15 @@
+// 클래스를 제네릭 하게 :)
+
+
 // either: a or b
 interface Either<L, R> {
   left: () => L;
   right: () => R;
 }
 
-class SimpleEither<L, R> implements Either<L, R> {
-  constructor(private leftValue: L, private rightValue: R) {}
+class SimpleEither<L, R> implements Either<L, R>{
+  constructor(private leftValue: L, private rightValue: R) { }
+
   left(): L {
     return this.leftValue;
   }
@@ -14,7 +18,9 @@ class SimpleEither<L, R> implements Either<L, R> {
     return this.rightValue;
   }
 }
+
 const either: Either<number, number> = new SimpleEither(4, 5);
 either.left(); // 4
-either.right(); //5
-const best = new SimpleEither({name: 'ellie'}, 'hello');
+either.right(); // 5
+
+const best = new SimpleEither({ name: 'yr' }, 'hello');
