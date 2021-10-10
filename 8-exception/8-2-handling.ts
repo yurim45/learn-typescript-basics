d212c706003128668e5a935b0ae50676542d3f35
yurim Kim <icho0405@gmail.com>
2021-10-10 10:06:47 +0900

    add: 우아하게 에러를 처리하는 방법

--- 8-exception/8-2-handling.ts
+++ 8-exception/8-2-handling.ts
@@ -1,34 +1,31 @@
-{
-  class TimeoutError extends Error {}
-  class OfflineError extends Error {}
-
-  class NetworkClient {
-    tryConnect(): void {
-      throw new OfflineError('no network!');
-    }
+class NetwortkClient {
+  tryConnect(): void {
+    throw new Error('no network!!!')
   }
-
-  class UserService {
-    constructor(private client: NetworkClient) {}
-
-    login() {
-      this.client.tryConnect();
-    }
-  }
-
-  class App {
-    constructor(private userService: UserService) {}
-    run() {
-      try {
-        this.userService.login();
-      } catch (error) {
-        // show dialog to use
-      }
-    }
-  }
-
-  const client = new NetworkClient();
-  const service = new UserService(client);
-  const app = new App(service);
-  app.run();
 }
+
+class UserService {
+  constructor(private client: NetwortkClient) { }
+
+  login() {
+    this.client.tryConnect();
+    // login...
+  }
+}
+
+class App {
+  constructor(private userService: UserService) { }
+  run() {
+    try {
+      this.userService.login();
+    } catch (error) {
+      this.userService.login();
+      console.log(`catched!! ${error}`);  // 의미 있는 곳에서 에러 처리하기
+    }
+  }
+}
+
+const client = new NetwortkClient();
+const service = new UserService(client);
+const app = new App(service);
+app.run();
