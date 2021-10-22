1a183f7c00aa395ab24aa62d41afe4bd62176615
yurim Kim <icho0405@gmail.com>
2021-10-23 08:36:44 +0900

    add: ReadOnly 😜

--- 9-advanced/9-4-condition.ts
+++ 9-advanced/9-4-condition.ts
@@ -1,4 +1,7 @@
-type Check<T> = T extends string ? boolean : number;
+// Conditional Type
+// 조건에 따라 Type을 지정할 수 있게 하는 것
+
+type Check<T> = T extends string ? boolean : number; // type Check이 기존에 주어진 타입<T>이 string을 상속한다면? boolean으로, 아니라면 number로 지정
 type Type = Check<string>; // boolean
 
 type TypeName<T> = T extends string
--- 9-advanced/9-5-readonly.ts
+++ 9-advanced/9-5-readonly.ts
@@ -1,10 +1,14 @@
 {
+  // ReadOnly 😜
+  // 읽을 수만 있는 타입. 불변성을 지키기 위해서 사용된다
+
+
   type ToDo = {
     title: string;
     description: string;
   };
 
   function display(todo: Readonly<ToDo>) {
-    // todo.title = 'jaja';
+    // todo.title = 'jaja';  // 보여주기만 하는 함수에서, 값을 수정하지 못하게 하기 위해 Readonly로 구현
   }
 }
