2deaa3a1d61cb104355c02afa30fb46019697be2
yurim Kim <icho0405@gmail.com>
2021-10-31 12:07:55 +0900

    add: module 데모

--- 10-javascript/10-3-module1.js
+++ 10-javascript/10-3-module1.js
@@ -1,4 +1,4 @@
-export default function add(a, b) {
+export default function add(a, b) {  // export할 때 default라는 키워드를 붙혀주면 import할 때 다른 이름을 사용해도 된다
   return a + b;
 }
 
@@ -7,6 +7,6 @@ export function add(a, b) {
 }
 
 export const number = 10;
-export function print() {
+export function print() {  // 한 파일에서 두 가지의 default를 사용할 수 없다
   console.log('print');
 }
