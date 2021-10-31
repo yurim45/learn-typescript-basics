02f3ebe465ea191f2dd4a3266691b29f1004f999
yurim Kim <icho0405@gmail.com>
2021-10-31 12:06:19 +0900

    add: module 데모

--- 10-javascript/10-3-module1.js
+++ 10-javascript/10-3-module1.js
@@ -1,3 +1,7 @@
+export default function add(a, b) {
+  return a + b;
+}
+
 export function add(a, b) {
   return a + b;
 }
--- 10-javascript/10-3-module2.js
+++ 10-javascript/10-3-module2.js
@@ -1,4 +1,9 @@
+// import sum, { print as printMessage } from './10-3-module1.js'; 
+// export default의 경우 이름 변경이 가능 add -> sum
+// import 할 때는 {} 안에 선언된 이름으로 가져와야 하고, 이름을 변경하고 싶다면 as라는 키워드로 변경할 수 있다
 import * as calculator from './10-3-module1.js';
+
 console.log(calculator.add(1, 2));
+
 calculator.print();
 calculator.number;
